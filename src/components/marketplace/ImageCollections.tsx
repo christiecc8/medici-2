df1bdd8c83ab0813a1b3e2341fb389ebdc82557a
christiecc8 <christie.cc8@gmail.com>
2022-08-20 17:20:28 -0400

    unformatted but integrated art collections

---
+++ src/components/marketplace/ImageCollections.tsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+import { getAllImageCollections } from '../../utils/retrieve';
+import '../../css/marketplace.css';
+import CollectionCard from './CollectionCard';
+
+interface ImageCollection {
+  artist: string,
+  name: string,
+  cover_cdn_thumbnail_url: string,
+  cover_cdn_url: string,
+  description: string,
+  resource_type: string
+}
+
+const ImageCollections: React.FC<{}> = () => {
+  const [imageCollections, setImageCollections] = useState<ImageCollection[]>();
+
+  useEffect(() => {
+    ( async () => {
+      if (!imageCollections) {
+      const res = await getAllImageCollections();
+      setImageCollections(res)
+      }
+    })();
+  }, [imageCollections, setImageCollections])
+
+  return (
+    <div className="grid grid-cols-3 z-20">
+    { imageCollections && imageCollections.map((col: ImageCollection, i) => (
+    <CollectionCard artist={col.artist} name={col.name} image={col.cover_cdn_thumbnail_url}/>
+    ))
+    }
+    </div>
+  );
+}
+export default ImageCollections
